05e6a345911ab516cc45b7ba35b05607980e329e
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-04 15:21:46 -0500

    Adds homepage pictures and button to choose player

--- src/main/java/com/detroitlabs/mortalkombatroshambo/Controllers/PlayerController.java
+++ src/main/java/com/detroitlabs/mortalkombatroshambo/Controllers/PlayerController.java
@@ -1,7 +1,21 @@
 package com.detroitlabs.mortalkombatroshambo.Controllers;
 
+import com.detroitlabs.mortalkombatroshambo.Repository.MortalKombatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PlayerController {
+
+    @Autowired
+    MortalKombatRepository mortalKombatRepository;
+
+    @RequestMapping("/")
+    public ModelAndView showHomePage(){
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("players", mortalKombatRepository.showAllMortalKombatCharacters());
+        return mv;
+    }
 }
