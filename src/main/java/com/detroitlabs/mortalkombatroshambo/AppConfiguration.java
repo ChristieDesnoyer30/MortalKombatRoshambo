f3944213cbb1f493a6360a01da5e0fa6737eec10
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-04 10:14:56 -0500

    adds architecture for Mortal Kombat Roshambo, .gitignore added as well

---
+++ src/main/java/com/detroitlabs/mortalkombatroshambo/AppConfiguration.java
@@ -0,0 +1,18 @@
+package com.detroitlabs.mortalkombatroshambo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class AppConfiguration {
+
+
+    public static void main(String [] args){
+        SpringApplication.run(AppConfiguration.class, args);
+
+
+    }
+}
